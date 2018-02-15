67cb3a7ddfa47baa03072fbffbece67bf8c7f15d
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-02-14 22:21:23 -0800

    finished lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
 })
 
 /*
@@ -10,8 +11,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	$('#colorBtn').click(randomizeColors);
+	$('a').click(addProjectDetails(e));
+
 }
 
 /*
@@ -26,5 +28,17 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
-}
+	$.get('project/' + idNumber, callback);
+	}
+
+	function callback(response) {
+		var tag = ".details#project" + response.id;
+		console.log(response.image);
+		var projectHTML = '<a href="#" class="thumbnail" style=" float: left; position: relative; width:30%;">' + 
+			'<img src="' + response.image + '" class="img"></a>'
+			+
+			'<p>' + response.date + '</p>' +
+			'<p><small>' + response.summary + '</small></p></a>';
+		$(tag).html(projectHTML)
+	}
+
